bcb9f8d5e6c53feae82ded9d00c2a255522ee001
Raemyn <Zhekalah@gmail.com>
2025-11-17 18:44:55 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Bambit-ts-Frontend/', // добавлено для GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
